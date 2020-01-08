3b0e054dd31a13b32e1a75e8b0db35d066973ec9
Theerapong Macbook <TheerapongMacbook>
2020-01-08 22:36:45 +0700

    add feature1

--- app/src/main/java/app/practice/example/MainActivity.kt
+++ app/src/main/java/app/practice/example/MainActivity.kt
@@ -1,7 +1,8 @@
 package app.practice.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btSayHello.setOnClickListener {
+            txtName.text = nameEditText.text.toString()
+        }
+
     }
-    
+
 }
