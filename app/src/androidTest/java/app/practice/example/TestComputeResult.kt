f75d16528bce322e5c7857f90643736d9774b5ce
Theerapong Macbook <TheerapongMacbook>
2020-01-11 09:52:48 +0700

    add viewmodel

---
+++ app/src/androidTest/java/app/practice/example/TestComputeResult.kt
@@ -0,0 +1,4 @@
+package app.practice.example;
+
+class TestComputeResult {
+}
---
+++ app/src/test/java/app/practice/example/MainViewModelTest.kt
@@ -0,0 +1,16 @@
+package app.practice.example
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+
+    @Test
+    fun getResult() {
+    }
+
+    @Test
+    fun computeResult() {
+    }
+}
