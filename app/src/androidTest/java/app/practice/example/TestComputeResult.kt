6265e1d098e800f6ff49ee4cd357645575418d85
Theerapong Macbook <TheerapongMacbook>
2020-01-11 09:53:01 +0700

    update add viewmodel

--- app/src/androidTest/java/app/practice/example/TestComputeResult.kt
+++ app/src/androidTest/java/app/practice/example/TestComputeResult.kt
@@ -1,4 +1,166 @@
-package app.practice.example;
+package app.practice.example
 
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.microsoft.appcenter.espresso.Factory
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class TestComputeResult {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var repoHelper = Factory.getReportHelper()
+
+    @Test
+    fun testComputeResult() {
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.editTxtMonthlySaving),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("1"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.editTxtInterestRate),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("2"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.editTxtYourCurrentAge),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("3"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.editTxtPlannedRetirementAge),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText("4"), closeSoftKeyboard())
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.editTxtCurrentSaving),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(replaceText("5"), closeSoftKeyboard())
+
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.btCalculate), withText("Calculate"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.txtResult),
+                withText("InterestedRate = 2.0, currentAge = 3, retirementAge = 4, monthlySaving = 1, currentSaving = 5"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("InterestedRate = 2.0, currentAge = 3, retirementAge = 4, monthlySaving = 1, currentSaving = 5")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    @After
+    fun testDown() {
+        repoHelper.label("Finishing Test")
+    }
 }
--- app/src/test/java/app/practice/example/MainViewModelTest.kt
+++ app/src/test/java/app/practice/example/MainViewModelTest.kt
@@ -1,16 +1,25 @@
 package app.practice.example
 
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class MainViewModelTest {
 
-    @Test
-    fun getResult() {
-    }
-
-    @Test
-    fun computeResult() {
-    }
+//    lateinit var viewModel: MainViewModel
+//
+//    @Before
+//    fun setup() {
+//        viewModel = MainViewModel()
+//    }
+//
+////    @Test
+////    fun getResult() {
+//////        viewModel.result
+////    }
+//
+//    @Test
+//    fun computeResult() {
+//        val res = viewModel.computeResult(1.0f, 2, 4, 5, 6)
+//        println("result = " + res)
+//    }
 }
